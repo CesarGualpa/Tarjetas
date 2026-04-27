fec08111832235b890306c231c34d8de52e2abf2
CesarGualpa <cesar_amagu@hotmail.com>
2026-04-27 16:00:12 -0500

    Se modifico la funcion pintarTarjetas

--- tarjetas.js
+++ tarjetas.js
@@ -1,9 +1,14 @@
-function crearTarjetas(){
-    let contenido="";
-    let divTarjetas=document.getElementById("divTarjetas")
-    for (let i=1; i<=5; i++){
-        contenido=contenido+"<div class='item'>" + i + "</div>"
-        console.log(contenido)
-        divTarjetas.innerHTML=contenido;
+function crearTarjetas() {
+    let contenido = "";
+
+    let desde = parseInt(document.getElementById("txtDesde").value);
+    let hasta = parseInt(document.getElementById("txtHasta").value);
+
+    let divTarjetas = document.getElementById("divTarjetas");
+
+    for (let i = desde; i <= hasta; i++) {
+        contenido = contenido + "<div class='item'>" + i + "</div>";
     }
+
+    divTarjetas.innerHTML = contenido;
 }
