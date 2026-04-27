1cded3ef14dac10a2342e7bf8681b5c5965d70cd
CesarGualpa <cesar_amagu@hotmail.com>
2026-04-27 16:08:46 -0500

    Se agrego una caja de txt, se modifico el js para haga un dalto dependiendo el numero

--- tarjetas.js
+++ tarjetas.js
@@ -3,10 +3,11 @@ function crearTarjetas() {
 
     let desde = parseInt(document.getElementById("txtDesde").value);
     let hasta = parseInt(document.getElementById("txtHasta").value);
+    let salto = parseInt(document.getElementById("txtSalto").value);
 
     let divTarjetas = document.getElementById("divTarjetas");
 
-    for (let i = desde; i <= hasta; i++) {
+    for (let i = desde; i <= hasta; i += salto) {
         contenido = contenido + "<div class='item'>" + i + "</div>";
     }
 
