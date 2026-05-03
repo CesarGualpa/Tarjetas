d1d189701c157cfa156ddb32e8b1ebae88411c87
CesarGualpa <cesar_amagu@hotmail.com>
2026-05-03 18:46:19 -0500

    Se cambion la estructura

--- tarjetas.js
+++ tarjetas.js
@@ -1,15 +1,31 @@
-function crearTarjetas() {
-    let contenido = "";
+function crearTarjetas(){
+    let contenido= "";
+    
+    let cmpDesde;
+    cmpDesde=document.getElementById("txtDesde");
+    let desde;
+    desde=cmpDesde.value;
+    let desdeEntero;
+    desdeEntero=parseInt(desde);
+    
+    let cmpHasta;
+    cmpHasta=document.getElementById("txtHasta");
+    let hasta;
+    hasta=cmpHasta.value;
+    let hastaEntero;
+    hastaEntero=parseInt(hasta);
 
-    let desde = parseInt(document.getElementById("txtDesde").value);
-    let hasta = parseInt(document.getElementById("txtHasta").value);
-    let salto = parseInt(document.getElementById("txtSalto").value);
+    let cmpSalto;
+    cmpSalto=document.getElementById("txtSalto");
+    let salto;
+    salto=cmpSalto.value;
+    let saltoEntero;
+    saltoEntero=parseInt(salto);
 
-    let divTarjetas = document.getElementById("divTarjetas");
-
-    for (let i = desde; i <= hasta; i += salto) {
-        contenido = contenido + "<div class='item'>" + i + "</div>";
+    let divTarjetas=document.getElementById("divTarjetas")
+    
+    for (let i=desdeEntero; i<=hastaEntero; i+=saltoEntero){
+        contenido=contenido + "<div class='item'>" +i+ "</div>";
     }
-
-    divTarjetas.innerHTML = contenido;
+    divTarjetas.innerHTML=contenido
 }
